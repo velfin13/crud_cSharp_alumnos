7192c073994938703bd4ed64503d9e9b4e40434a
Velfin Velasquez <velfinvelasquez@gmail.com>
2024-01-28 20:22:56 -0500

    data

---
+++ db/db.sql
@@ -0,0 +1,20 @@
+CREATE TABLE alumnos (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    nombre varchar(50) NOT NULL,
+    apellido varchar(50) NOT NULL
+);
+
+INSERT INTO
+    alumnos
+values
+    (null, 'Velfin', 'Velasquez');
+
+INSERT INTO
+    alumnos
+values
+(null, 'Andre', 'Quiro');
+
+INSERT INTO
+    alumnos
+values
+(null, 'Kat', 'Loor');
